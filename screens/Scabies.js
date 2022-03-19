8568e35f073e3b4bec4cd2b85b0b46035f0d788e
jon06041 <jon0604@mac.com>
2022-03-19 12:48:20 -0400

    3-19-22 3

--- screens/Scabies.js
+++ screens/Scabies.js
@@ -15,15 +15,49 @@ const MyComponent = () => {
   //const handlePress = () => setExpanded(!expanded);
 
   return (
-    <List.Section title="Accordions">
+    <List.Section title="Treatment Options">
       <List.Accordion
-        title="Uncontrolled Accordion"
+        title="RECOMMENDED REGIMEN"
         left={props => <List.Icon {...props} icon="folder" />}>
-        <List.Item title="First item" />
-        <List.Item title="Second item" />
+        <List.Item  />
+        <View>
+          <Text>
+          permethrin 5% cream applied to all areas of the body (from neck down), wash after 8–14 hours21 OR
+          </Text>
+        
+
+        </View>
+        <List.Item  />
+        <View>
+          <Text>
+
+          ivermectin 200ug/kg body weight orally, repeated in 14 days22 OR
+          </Text>
+
+        </View>
+        <List.Item  />
+        <View>
+          <Text>
+
+          ivermectin 1% lotion applied to all areas of the body (from neck down), wash after 8–14 hours; repeat treatment in 1 week if symptoms persist
+          </Text>
+
+        </View>
       </List.Accordion>
 
-     
+      <List.Accordion
+        title="ALTERNATIVE REGIMEN"
+        left={props => <List.Icon {...props} icon="folder" />}>
+        <List.Item  />
+        <View>
+          <Text>
+
+          lindane 1% 1 oz of lotion or 30 gm of cream applied thinly to all areas of the body (from neck down), wash after 8 hours23
+          </Text>
+
+        </View>
+        
+      </List.Accordion>
     </List.Section>
   );
 };
