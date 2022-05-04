8291a3b13c371308c8540e8af3bf1f543c9fadef
jon06041 <jon0604@mac.com>
2022-05-03 20:29:10 -0400

    5-3-22

--- screens/MDL.js
+++ screens/MDL.js
@@ -1,6 +1,6 @@
 
 
-import {  Text, View, ScrollView, Image } from 'react-native';
+import {  Stylesheet,Text, View, ScrollView, Image, fullScreenWidth,fullScreenHeight,Dimensions } from 'react-native';
 import * as React from 'react';
 import styles from '../src/styles';
 
@@ -12,18 +12,27 @@ function MDL ({navigation}){
     Medical Diagnostic Laboratories, L.L.C. (MDL), founded in 1997, serves primarily as a reference laboratory for Polymerase Chain Reaction (PCR) based testing to physicians, laboratories and hospitals worldwide.
      MDL is a CLIA certified infectious disease laboratory with multiple state licenses specializing in PCR technology. Molecular diagnostic techniques, such as the PCR method, are considered to be the most sensitive and specific for the laboratory diagnosis of infectious diseases. 
      MDL offers unique, non-invasive, highly stable specimen collection and transport platforms, such as the <Text style={styles.innerTextred}>One</Text>Swab,  
-    <Text style={styles.innerTextyellow}> Uro</Text>Swab and <Text style={styles.innerTextgreen}>Naso</Text>Swab These platforms enable MDL to offer a variety of PCR based tests from a single swab with high sensitivity and specificity
+    <Text style={styles.innerTextyellow}> Uro</Text>Swab and <Text style={styles.innerTextgreen}>Naso</Text>Swab These platforms enable MDL to offer a variety of PCR based tests from a single swab with high sensitivity and specificity</Text>
+
     
-</Text>
-<View
-            style={{ flex: 1, alignItems: "center", flexDirection: "row",justifyContent: "center" }}
-          >
-              <Image style={{ flex: 1 , justifyContent: "center"}}
-      source={require("../screens/images/mdl-genesis.png")}
-      />
-          </View>
-</ScrollView>
-</View>
+    <View>
+      <Image source={require("../screens/images/mdl-genesis.png")}
+        resizeMode='contain'
+        style={{
+          
+          }} />
+    
+   
+   </View>
+   </ScrollView>
+   </View>
+
+
+
+
+ 
+
+
 
 
 
